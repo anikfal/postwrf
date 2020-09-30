0e20d8dcb435dd33df080e09dd3a225f3bba4050
Amirhossein Nikfal <ah.nikfal@gmail.com>
2020-09-30 21:24:34 +0330

    Multiple WRF files - GeoTIFF

--- modules/geotiff.sh
+++ modules/geotiff.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 #PostWRF Version 1.1 (Apr 2020)
 #Coded by "Amirhossein Nikfal" <ah.nikfal@gmail.com>, <anik@ut.ac.ir>
-curdir=`pwd`
-trap 'my_exit; exit' SIGINT SIGQUIT
-count=0
-my_exit()
-{
-curdir2=`pwd`
-rm timestep_file 2> /dev/null
-if [[ $curdir != $curdir2 ]]; then
 
-wrflist=`ls wrfout*`
-totnom=`echo $wrflist | wc -w`
-rmcounter=1
-while [ $rmcounter -le $totnom ]; do
-file=`echo $wrflist | cut -d' ' -f$rmcounter`
- if [[ -L "$file" ]]; then
- rm $file 2> /dev/null
- fi
-rmcounter=$((rmcounter+1))
-done
-unset rmcounter
-fi
+trap 'my_exit; exit' SIGINT SIGQUIT
+my_exit() {
+rm $outputdir/*.ncl $outputdir/postwrf_wrfout* $outputdir/.AllWRFVariables $outputdir/eqname 2>/dev/null
+rm $outputdir/totalequation.txt $outputdir/variables*.txt $outputdir/equnit $postwrf_dir/modules/postwrf_wrfout* 2>/dev/null
+rm $postwrf_dir/*.ncl $postwrf_dir/postwrf_wrfout* $postwrf_dir/.AllWRFVariables $postwrf_dir/eqname 2>/dev/null
+rm $postwrf_dir/totalequation.txt $postwrf_dir/variables.txt $postwrf_dir/equnit 2>/dev/null
 }
 
 if [ ${average_onoff} != "1" ]; then
-       wrfout2=`echo $wrfout | awk -F/ '{print $NF}'`  #For naming, NCL must be run by wrfout, not wrfout2
+      #  wrfout2=`echo $wrfout | awk -F/ '{print $NF}'`  #For naming, NCL must be run by wrfout, not wrfout2
        ncl -Q modules/timestep.ncl > timestep_file
        echo " "`tail -n 1 timestep_file | cut -d " " -f 2-`
        rm timestep_file
@@ -37,13 +23,19 @@ if [ ${average_onoff} != "1" ]; then
        export tstep
 fi
 
-wrfout2=`echo $wrfout | awk -F/ '{print $NF}'`  #For naming, NCL must be run by wrfout, not wrfout2
+wrftemp=( $( ls $postwrf_dir/postwrf_wrfout* ) )
+wrfout2=`basename ${wrftemp[0]}` #In case of mulitfiles, pick the first file for naming
 echo -e "\n---------------------------------------------------------------"
 mkdir -p outputs_$wrfout2
 ln -sf $postwrf_dir/.AllWRFVariables $postwrf_dir/modules
 cd outputs_$wrfout2
-ln -sf ../wrfout* .
-ln -sf ../.AllWRFVariables .
+export outputdir=`pwd`
+ln -sf $postwrf_dir/.AllWRFVariables $postwrf_dir/modules
+ln -sf $postwrf_dir/.AllWRFVariables .
+ln -sf $postwrf_dir/postwrf_wrfout* $postwrf_dir/modules
+ln -sf $postwrf_dir/postwrf_wrfout* .
+ln -sf $postwrf_dir/modules/geotiff.ncl .
+ln -sf $postwrf_dir/modules/read_wrfouts.ncl .
 echo "Converting to Geotiff. Please wait ..."
 ncl -Q $postwrf_dir/modules/geotiff.ncl
 geotiff_resume=`cat $postwrf_dir/modules/geo_eq_ok.txt`
@@ -57,7 +49,11 @@ if [[ $geotiff_resume == "True" ]]; then
     rm $tiffile".tiff" 2> /dev/null
   done
   rm *.nc 2> /dev/null
-  rm wrfout* 2> /dev/null
-  rm -f .AllWRFVariables 2> /dev/null
-  rm -f *.txt 2> /dev/null
+  # rm wrfout* 2> /dev/null
+  # rm -f .AllWRFVariables 2> /dev/null
+  # rm -f *.txt 2> /dev/null
 fi
+rm $outputdir/*.ncl $outputdir/postwrf_wrfout* $outputdir/.AllWRFVariables $outputdir/eqname   2>/dev/null
+rm $outputdir/totalequation.txt $outputdir/variables*.txt $outputdir/equnit $postwrf_dir/modules/postwrf_wrfout* 2>/dev/null
+rm $postwrf_dir/*.ncl $postwrf_dir/postwrf_wrfout* $postwrf_dir/.AllWRFVariables $postwrf_dir/eqname   2>/dev/null
+rm $postwrf_dir/totalequation.txt $postwrf_dir/variables.txt $postwrf_dir/equnit 2>/dev/null
