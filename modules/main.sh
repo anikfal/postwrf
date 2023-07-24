48656bc4adf805edc09d009a5b4552bf8cc0a538
Amir H. Nikfal <ah.nikfal@gmail.com>
2023-07-24 17:15:42 +0200

    resolve the problem caused by space as the last name/value (Extraction section)

--- modules/main.sh
+++ modules/main.sh
@@ -34,8 +34,8 @@ my_exit() {
 
 function countline() {
   numlinevars=$(sed -n "/$myvar/p" namelist.wrf | awk -F"=" '{print $NF}' | awk -F',' '{ print NF }')
-  ifendcomma=$(sed -n "/$myvar/p" namelist.wrf | awk -F"=" '{print $NF}' | rev | cut -c1)
-  if [[ $ifendcomma == "," ]]; then
+  ifendcomma=$(sed -n "/$myvar/p" namelist.wrf | awk -F"=" '{print $NF}' | awk -F "," '{print $NF}' | tr -d " ")
+  if [[ $ifendcomma == "" ]]; then
     numlinevars=$((numlinevars - 1))
   fi
 }
